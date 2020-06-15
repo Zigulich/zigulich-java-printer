bea8228a128e1d94dcb0a5bac279715611eed65f
Luan Zigulich <39498496+Zigulich@users.noreply.github.com>
2020-06-15 08:54:01 -0300

    fix: tamanho incorreto de caracteres na DARUMA

--- src/main/java/com/zigulich/classeimpressao/DARUMA.java
+++ src/main/java/com/zigulich/classeimpressao/DARUMA.java
@@ -18,7 +18,7 @@ public class DARUMA {
     // Parâmetros genéricos da impressora
     public static int CARACTERES_EXPANDIDO  = 24;
     public static int CARACTERES_NORMAL     = 48; 
-    public static int CARACTERES_CONDENSADO = 57; 
+    public static int CARACTERES_CONDENSADO = 64; 
     
     public String subsTexto(String t){
         String Receive = Normalizer.normalize(t, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
